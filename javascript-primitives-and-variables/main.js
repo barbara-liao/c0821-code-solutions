7c3f833ca71e6a38497d4d0550d3655d7be74787
Barbara Liao <barbara.liao3@gmail.com>
2021-08-23 10:43:37 -0700

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+var fullName = 'Barbara Liao';
+var isCool = true;
+var totalPets = 0;
+
+console.log('Value of Full Name:', fullName);
+console.log('Value of Is Cool:', isCool);
+console.log('Value of Total Pets: ', totalPets);
+
+console.log('Type of fullName: ', typeof fullName);
+console.log('Type of isCool: ', typeof isCool);
+console.log('Type of totalPets: ', typeof totalPets);
+
+var unknown = null;
+console.log('Value of unknown: ', unknown);
+console.log('Type of unknown: ', typeof unknown);
+
+var never;
+console.log('Value of never: ', never);
+console.log('Type of never: ', typeof never);
