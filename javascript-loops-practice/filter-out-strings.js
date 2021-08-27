73d69251d5e1cac7e6260def66a21e30311cd90b
Barbara Liao <barbara.liao3@gmail.com>
2021-08-27 09:57:57 -0700

    Updated javascript-looops-practice exercise.

--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -2,7 +2,7 @@
 function filterOutStrings(values) {
   var output = [];
   for (var i = 0; i < values.length; i++) {
-    if ((typeof values[i]) !== (typeof 'string')) {
+    if ((typeof values[i]) !== (typeof '')) {
       output.push(values[i]);
     }
   }
