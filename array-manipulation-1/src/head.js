6869ff714e82e7594f3c1cf1329f880ee3bc83cb
Barbara Liao <barbara.liao3@gmail.com>
2021-08-27 20:35:14 -0700

    Completed array-manipulation-exercise.

---
+++ array-manipulation-1/src/head.js
@@ -0,0 +1,15 @@
+/* exported head */
+
+/*
+- create storage for output
+- look at array
+- locate the first item of the array
+- push it to the output
+- return output
+*/
+
+function head(array) {
+  var output = '';
+  output = array[0];
+  return output;
+}
