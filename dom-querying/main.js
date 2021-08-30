9914d11ee7482e4f239bcabd1f7faf125cd3a807
Barbara Liao <barbara.liao3@gmail.com>
2021-08-30 10:43:38 -0700

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,20 @@
+var h1Selector = document.querySelector('h1');
+console.log(h1Selector);
+
+console.dir(h1Selector);
+
+var idSelector = document.querySelector('#explanation');
+console.log(idSelector);
+
+console.dir(idSelector);
+
+var classSelector = document.querySelector('.hint');
+console.log(classSelector);
+
+console.dir(classSelector);
+
+var pSelectorAll = document.querySelectorAll('p');
+console.log(pSelectorAll);
+
+var classSelectorAll = document.querySelectorAll('.example-link');
+console.log(classSelectorAll);
