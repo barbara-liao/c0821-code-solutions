f1a861a37cdbd1dff34c8a8beaab98a339d9174c
Barbara Liao <barbara.liao3@gmail.com>
2021-08-31 17:54:57 -0700

    In Progress.

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,21 @@
+/*
+- go through each element one by one
+- for the selected element, underline it
+- if the correct letter is typed,
+//   - the element will turn green and go to the next element
+// - if an incorrect letter is typed,
+//   - the element will turn red and will not continue
+// */
+
+// function type(event) {
+//   if ()
+// }
+
+// for (var i = 0; i > event.length; i++) {
+//   if (i)
+// }
+
+// var $span = document.querySelectorAll('span');
+// var $borderBottom = document.
+
+// document.addEventListener('keydown', type);
