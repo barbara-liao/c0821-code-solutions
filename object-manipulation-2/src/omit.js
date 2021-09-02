aff132fa4653119af0559db700ff0e7da03a4af9
Barbara Liao <barbara.liao3@gmail.com>
2021-09-02 10:05:55 -0700

    In Progress.

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,23 @@
 /* exported omit */
+
+/*
+- create storage for output
+- check keys one by one
+- if key in object does not equal any key in keys
+  - push to output
+- else,
+  - return output empty
+- return output
+*/
+
+// function omit(source, keys) {
+//   var output = {};
+//   for (var i = 0; i < keys.length; i++) {
+//     if (!(source.key in keys)) {
+//       output[keys[i]] = source[keys[i]];
+//     } else {
+//       output;
+//     }
+//   }
+//   return output;
+// }
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -10,12 +10,16 @@
 
 function pick(source, keys) {
   var output = {};
-  var i = 0;
-  for (keys[i] in source) {
-    output[keys[i]] = source[keys[i]];
-    i++;
+  for (var i = 0; i < keys.length; i++) {
+    // debugger;
+    if (keys[i] in source) {
+      output[keys[i]] = source[keys[i]];
+    } else {
+      return output;
+    }
   }
   return output;
 }
 
-pick({ foo: 1, bar: 2, baz: 3 }, ['foo', 'baz']);
+pick({ qux: 4, corge: 5 }, ['bar', 'grault']);
+pick({ bar: 2 }, ['foo', 'bar', 'baz']);
