789c044c7b3f2f3da2003c39c753913483c26a7d
Barbara Liao <barbara.liao3@gmail.com>
2021-09-05 19:03:07 -0700

    in progress

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -47,3 +47,7 @@ omit({ foo: 1, bar: 2, baz: 3 }, ['foo', 'baz']); // -> { bar: 2 }
 //   }
 //   return output;
 // }
+
+/*
+Hey Barbara! So in that interior for loop where you are looping over the list of keys to omit, you are assigning the current key in the outer for...in loop every iteration, if the keys[i] value doesn't match sourceKeys, but if the array is any more than one item long, even if one of those items is the thing to avoid and you break, it will still get assigned to the output if it wasn't the first key in the array. What you really want to do is loop over the array to see if the value is in it, and after the loop is done, if there was no value in the array that matches the current sourceKeys value, you can assign that value to the output.
+*/
