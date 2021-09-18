c2c433856ca96571771f7c08d2e4acabe9fcb12b
Barbara Liao <barbara.liao3@gmail.com>
2021-09-18 14:36:00 -0700

    Completed string-manipulation-3 exercise.

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,5 +1,43 @@
 /* exported isAnagram */
 
 /*
--
+- create storage for temporary first string
+- create storage for temporary second string
+- look at the letters in both string one by one
+- push first string with no spaces into temporary storage
+- push second string with no spaces into temporary storage
+- if first and second string are not the same length, then return false
+- sort first and second string to be in alphabetical order
+- look at each letter of the strings one by one
+  - compare each letter with the other and if the letter in the first string in position i doesn't match the letter i in the second string
+  - return false
+- otherwise, return true
 */
+
+function isAnagram(firstString, secondString) {
+  var firstStringStorage = [];
+  var secondStringStorage = [];
+
+  for (var i = 0; i < firstString.length; i++) {
+    if (firstString[i] !== ' ') {
+      firstStringStorage.push(firstString[i]);
+    }
+  }
+
+  for (var a = 0; a < secondString.length; a++) {
+    if (secondString[a] !== ' ') {
+      secondStringStorage.push(secondString[a]);
+    }
+  }
+
+  firstStringStorage.sort();
+  secondStringStorage.sort();
+
+  for (var b = 0; b < firstStringStorage.length; b++) {
+    if (firstStringStorage[b] !== secondStringStorage[b]) {
+      return false;
+    }
+  }
+
+  return true;
+}
--- string-manipulation-3/src/is-palindromic.js
+++ string-manipulation-3/src/is-palindromic.js
@@ -1,21 +1,30 @@
 /* exported isPalindromic */
 
 /*
+- create storage for temporary output
 - look at each character one by one
-- if the characters going forward equals the characters in reverse, except for the spaces
+- if the letter is not a space
+  - add to temporary output
+- look at the temporary output one by one
+  - if the characters going forward equals the characters in reverse
   - return true
 - otherwise,
   - return false
 */
 
 function isPalindromic(string) {
-  for (var i = 0; i < string.length; i++) {
-    // debugger;
-    if (string[i] !== string[string.length - 1 - i]) {
+  var stringOutput = '';
+
+  for (var a = 0; a < string.length; a++) {
+    if (string[a] !== ' ') {
+      stringOutput += string[a];
+    }
+  }
+
+  for (var i = 0; i < stringOutput.length; i++) {
+    if (stringOutput[i] !== stringOutput[stringOutput.length - 1 - i]) {
       return false;
     }
   }
   return true;
 }
-
-isPalindromic('rasecar');
