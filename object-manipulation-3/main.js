6c04991f00d29b7a90e158fbbee87f2cd25262f3
Barbara Liao <barbara.liao3@gmail.com>
2021-09-28 22:35:45 -0700

    In progress.

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,20 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+
+var players = [
+  {
+    name: 'Ron',
+    hand: []
+  },
+  {
+    name: 'Leslie',
+    hand: []
+  },
+  {
+    name: 'Ben',
+    hand: []
+  },
+  {
+    name: 'April',
+    hand: []
+  }
+];
