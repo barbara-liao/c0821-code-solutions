33437998a8e2fa9289da2eb2d85518273e953e6b
Barbara Liao <barbara.liao3@gmail.com>
2021-10-11 10:21:31 -0700

    Updated creation of card deck.

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -19,64 +19,28 @@ var players = [
   }
 ];
 
-var cardDeck = [
-  { rank: 'Ace', suit: 'clubs' },
-  { rank: 2, suit: 'clubs' },
-  { rank: 3, suit: 'clubs' },
-  { rank: 4, suit: 'clubs' },
-  { rank: 5, suit: 'clubs' },
-  { rank: 6, suit: 'clubs' },
-  { rank: 7, suit: 'clubs' },
-  { rank: 8, suit: 'clubs' },
-  { rank: 9, suit: 'clubs' },
-  { rank: 10, suit: 'clubs' },
-  { rank: 'Jack', suit: 'clubs' },
-  { rank: 'Queen', suit: 'clubs' },
-  { rank: 'King', suit: 'clubs' },
-  { rank: 'Ace', suit: 'diamonds' },
-  { rank: 2, suit: 'diamonds' },
-  { rank: 3, suit: 'diamonds' },
-  { rank: 4, suit: 'diamonds' },
-  { rank: 5, suit: 'diamonds' },
-  { rank: 6, suit: 'diamonds' },
-  { rank: 7, suit: 'diamonds' },
-  { rank: 8, suit: 'diamonds' },
-  { rank: 9, suit: 'diamonds' },
-  { rank: 10, suit: 'diamonds' },
-  { rank: 'Jack', suit: 'diamonds' },
-  { rank: 'Queen', suit: 'diamonds' },
-  { rank: 'King', suit: 'diamonds' },
-  { rank: 'Ace', suit: 'hearts' },
-  { rank: 2, suit: 'hearts' },
-  { rank: 3, suit: 'hearts' },
-  { rank: 4, suit: 'hearts' },
-  { rank: 5, suit: 'hearts' },
-  { rank: 6, suit: 'hearts' },
-  { rank: 7, suit: 'hearts' },
-  { rank: 8, suit: 'hearts' },
-  { rank: 9, suit: 'hearts' },
-  { rank: 10, suit: 'hearts' },
-  { rank: 'Jack', suit: 'hearts' },
-  { rank: 'Queen', suit: 'hearts' },
-  { rank: 'King', suit: 'hearts' },
-  { rank: 'Ace', suit: 'spades' },
-  { rank: 2, suit: 'spades' },
-  { rank: 3, suit: 'spades' },
-  { rank: 4, suit: 'spades' },
-  { rank: 5, suit: 'spades' },
-  { rank: 6, suit: 'spades' },
-  { rank: 7, suit: 'spades' },
-  { rank: 8, suit: 'spades' },
-  { rank: 9, suit: 'spades' },
-  { rank: 10, suit: 'spades' },
-  { rank: 'Jack', suit: 'spades' },
-  { rank: 'Queen', suit: 'spades' },
-  { rank: 'King', suit: 'spades' }
-];
+var cardDeck = [];
+
+function createCards() {
+  var suit = ['clubs', 'diamonds', 'hearts', 'spades'];
+  var rank = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+  var card = {};
+  for (var s = 0; s < suit.length; s++) {
+    for (var r = 0; r < rank.length; r++) {
+      card.suit = suit[s];
+      card.rank = rank[r];
+      cardDeck.push(card);
+      card = {};
+    }
+  }
+}
+
+createCards();
 
 var shuffleDeck = _.shuffle(cardDeck);
 
 function dealCards(player) {
+  console.log(cardDeck);
   for (var i = 0; i < 2; i++) {
     player.hand.push(shuffleDeck[i]);
     shuffleDeck.splice(0, 1);
@@ -108,16 +72,6 @@ function calculateScore(allPlayers) {
   }
   return players;
 }
-
-/*
-- to find the largest number
-- create storage for output
-- make the output equal to the first person
-- look at each person starting at the second one
-- if the score is larger than the one in the output,
-  - reassign the output
-- return the output
-*/
 
 function calculateWinner(allPlayers) {
   calculateScore(allPlayers);
