4f0b53bbbe6850aed04df5e1874618661c63ba32
Barbara Liao <barbara.liao3@gmail.com>
2021-10-15 12:38:54 -0700

    Updated conditional.

--- array-filter/filter.js
+++ array-filter/filter.js
@@ -1,7 +1,7 @@
 function filter(array, predicate) {
   const output = [];
   for (let i = 0; i < array.length; i++) {
-    if (predicate(array[i]) === true) {
+    if (predicate(array[i])) {
       output.push(array[i]);
     }
   }
