55fd7abb24a51c09441a360364838e3c01c533ae
Barbara Liao <barbara.liao3@gmail.com>
2021-10-18 16:32:11 -0700

    Completed react-elements exercise.

---
+++ react-elements/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const message = React.createElement('h1', null, 'Hello, React!');
+ReactDOM.render(message, document.getElementById('root'));
