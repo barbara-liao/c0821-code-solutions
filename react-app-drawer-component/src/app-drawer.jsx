ec5dfdac9b3b17caa3c2daed6fdd49e2f4dd441f
Barbara Liao <barbara.liao3@gmail.com>
2021-10-21 15:16:13 -0700

    In progress.

---
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class AppDrawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMenu: false
+    };
+  }
+
+}
