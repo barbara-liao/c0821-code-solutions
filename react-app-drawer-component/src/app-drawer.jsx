df0d2e529078d68d25bb101d5e347523208da058
Barbara Liao <barbara.liao3@gmail.com>
2021-10-22 12:49:53 -0700

    Completed react-app-drawer-component challenge.

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -6,6 +6,41 @@ export default class AppDrawer extends React.Component {
     this.state = {
       showMenu: false
     };
+    this.handleOpenMenu = this.handleOpenMenu.bind(this);
+    this.handleSelectItem = this.handleSelectItem.bind(this);
   }
 
+  handleOpenMenu() {
+    this.setState({ showMenu: true });
+  }
+
+  handleSelectItem() {
+    this.setState({ showMenu: false });
+  }
+
+  modalHidden() {
+    return (this.state.showMenu ? '' : 'hidden');
+
+  }
+
+  render() {
+    const isHidden = this.modalHidden();
+    return (
+      <div>
+        <div onClick={this.handleSelectItem} className={`modal-overlay ${isHidden}`}>
+          <div className="modal flex">
+            <h2 className="menu-padding">Choose a Pokemon</h2>
+            <button onClick={this.handleSelectItem} className="flex-start menu-padding">Bulbasaur</button>
+            <button onClick={this.handleSelectItem} className="flex-start menu-padding">Charmander</button>
+            <button onClick={this.handleSelectItem} className="flex-start menu-padding">Squirtle</button>
+          </div>
+        </div>
+        <div>
+          <button onClick={this.handleOpenMenu} className="icon-padding">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
